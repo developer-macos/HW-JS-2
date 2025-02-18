2063d0a9b4d25243fa3f91429394c4a9bc9959f5
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2025-02-18 20:12:53 +0100

    Finished

---
+++ index.js
@@ -0,0 +1,47 @@
+// let C = prompt("Give me a number to put in the Fahrenheit.");
+
+// alert(C * (9 / 5) + 32);
+
+
+let currentMonth = 1;
+
+console.log(currentMonth = 30 * 24);
+
+currentHealth = 100
+
+currentStamina = 120
+
+console.log(currentHealth - 10);
+console.log(currentStamina - 10);
+
+currentDiscount = 10 * 1;
+
+currentSpend = 100;
+
+console.log(currentSpend - currentDiscount);
+
+let randomNumber = 1.25222245455555852
+
+console.log(Math.floor(randomNumber));
+
+let number = 1211151;
+
+console.log(parseFloat(Number));
+
+let row = 121.002544;;
+
+console.log(parseInt(row));
+
+let numberLL = 25;
+
+console.log(Math.sqrt(numberLL));
+
+
+let newNumber = 12.001255;
+
+console.log(parseInt(newNumber));
+toString(newNumber)
+
+
+
+
